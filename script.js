d1d7cf7dac26ce79efe9416f82079cd2bae8af7e
Nicholas Duffew <pbsvimqx@students.codeinstitute.net>
2024-10-30 08:04:34 +0000

    add comments to arrow functions

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+// arrow functions with paramters
 const addTwoNumbers = (a,b) => {
     // code block
     return a + b;
@@ -6,16 +8,20 @@ const addTwoNumbers = (a,b) => {
 let sum = addTwoNumbers(3, 8);
 console.log(sum);
 
+// single line arrow functions with parameters
+// also valid (required with multiline returns)
 const addTwoNumbers2 = (a, b) => a + b;
 let sum2 = addTwoNumbers2(7, 9);
 console.log(sum2);
 
+// implicit returns
 const saySomething = message => console.log(message)
 saySomething("Hi there!");
 
 const sayHello = () => console.log("Hi!");
 sayHello();
 
+// returning multiple lines
 const returnMultipleLines = () => (
     `<p>
     This is a mulitiline string.
