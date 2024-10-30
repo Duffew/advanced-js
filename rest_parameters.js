6e04147e507d32ce26723f1a1f3865b0be503e5c
Nicholas Duffew <pbsvimqx@students.codeinstitute.net>
2024-10-30 19:35:10 +0000

    add rest_parameters.js

---
+++ rest_parameters.js
@@ -0,0 +1,23 @@
+// regular function call
+const sumAll = (a, b, c) => a + b + c;
+let sum = sumAll(1, 2, 3);
+console.log("Sum", sum);
+
+/* the above function can olnt take 3 arguments to represent parameters a, b and c
+extra argumements are ignored e.g.: */
+
+let sum2 = sumAll(1, 2, 3, 4, 5, 6)
+console.log("Sum2", sum2);
+
+// function using ...rest
+const sumRest = (a, b, c, ...rest) => {
+    let sum = a + b + c;
+    for (let i of rest) {
+        sum += i;
+    }
+
+    return sum
+}
+
+let sum3 = sumRest(1, 2, 3, 4, 5, 6)
+console.log("Sum3", sum3)
